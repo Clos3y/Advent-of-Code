0bf4b5a0a896c81c2ecb0c2f0a193107826a0a3c
Clos3y <Clos3y@users.noreply.github.com>
2021-12-06 15:24:13 +0000

    autopep8 action fixes

--- Day6/daysix.py
+++ Day6/daysix.py
@@ -1,19 +1,22 @@
 import numpy as np
 
-def fish_takeover(days : int, file : str) -> int:
-	with open(file,"r") as fish_data:
-		fish = fish_data.read()
-		fish = np.fromiter(map(lambda x: np.byte(x),fish[0::2]),dtype=np.byte)
 
-		for i in range(days):
+def fish_takeover(days: int, file: str) -> int:
+    with open(file, "r") as fish_data:
+        fish = fish_data.read()
+        fish = np.fromiter(
+            map(lambda x: np.byte(x), fish[0::2]), dtype=np.byte)
 
-			for k,j in np.ndenumerate(fish):
-				if j - 1 >= 0:
-					fish[k] -= 1
-				else:
-					fish[k] = 6
-					fish = np.append(fish,8)
+        for i in range(days):
 
-		return fish.size
+            for k, j in np.ndenumerate(fish):
+                if j - 1 >= 0:
+                    fish[k] -= 1
+                else:
+                    fish[k] = 6
+                    fish = np.append(fish, 8)
 
-print(fish_takeover(256,"input.txt"))
+        return fish.size
+
+
+print(fish_takeover(256, "input.txt"))
